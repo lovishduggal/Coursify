05a0fac2dad05b2bed3a0e93e039e28c36f74f96
Lovish Duggal <lovishduggal11@gmail.com>
2023-11-15 07:49:05 +0530

    Created Authentication Middleware

--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -1,10 +1,17 @@
 import express from 'express';
-import { login, logout, register } from '../Controllers/userController.js';
+import {
+    getMyProfile,
+    login,
+    logout,
+    register,
+} from '../Controllers/userController.js';
+import { isAuthenticated } from '../Middlewares/auth.js';
 
 const router = express.Router();
 
 router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/logout').get(logout);
+router.route('/me').get(isAuthenticated, getMyProfile);
 
 export default router;
--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ import { config } from 'dotenv';
 import { ErrorMiddleware } from './Middlewares/Error.js';
 import course from './Routes/courseRoutes.js';
 import user from './Routes/userRoutes.js';
+import cookieParser from 'cookie-parser';
 config({
     path: './config/.env',
 });
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use('/api/v1', course);
 app.use('/api/v1', user);
